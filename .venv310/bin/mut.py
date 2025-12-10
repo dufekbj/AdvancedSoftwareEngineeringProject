882e212635276e47bed14f3912135e2a9f1e1856
brjdufek14 <brjdufek14@gmail.com>
2025-12-09 20:25:19 -0500

    Updated the gitignore to include the virtual environment folders.

--- .venv310/bin/mut.py
+++
@@ -1,7 +0,0 @@
-#!/Users/braddufek/AdvSWEng/AdvancedSoftwareEngineeringProject/.venv310/bin/python3.10
-
-import sys
-from mutpy import commandline
-
-if __name__ == '__main__':
-    commandline.main(sys.argv)
